7692a45c07bef772ac56eb2589cc0ba785716c4e
Tapishagrawal <tapishg4@gmail.com>
2023-10-30 16:45:12 +0530

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { connection } = require('./db');
 const { userRouter } = require('./routes/users.route');
 const { postRouter } = require('./routes/posts.route');
+const cors = require("cors")
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use("/users", userRouter)
 app.use("/posts", postRouter)
